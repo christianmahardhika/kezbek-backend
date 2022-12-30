bdd587f4066be8ae19f3f096dcdb96859a996b96
Christian <christian.mahardhika@gmail.com>
2022-12-30 22:07:39 +0700

    fix import dependency to test file

--- src/healthcheck/healthcheck.controller.spec.ts
+++ src/healthcheck/healthcheck.controller.spec.ts
@@ -1,4 +1,7 @@
+import { TerminusModule } from '@nestjs/terminus';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configuration } from '../config/config';
 import { HealthcheckController } from './healthcheck.controller';
 import { HealthcheckService } from './healthcheck.service';
 
@@ -9,6 +12,19 @@ describe('HealthcheckController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthcheckController],
       providers: [HealthcheckService],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: configuration.GetPostgresConfig().host,
+          port: configuration.GetPostgresConfig().port,
+          username: configuration.GetPostgresConfig().username,
+          password: configuration.GetPostgresConfig().password,
+          database: configuration.GetPostgresConfig().database,
+          synchronize: configuration.GetPostgresConfig().synchronize,
+          autoLoadEntities: true,
+        }),
+        TerminusModule,
+      ],
     }).compile();
 
     controller = module.get<HealthcheckController>(HealthcheckController);
